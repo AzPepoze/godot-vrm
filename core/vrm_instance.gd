4f653f564f87f48a6ec5370c5be8b0e56e07e77a
AzPepoze <azpepoze@gmail.com>
2026-05-31 01:38:17 +0700

    refactor: replace custom property management with synchronized exported arrays and add simulator recreation utility

--- core/vrm_instance.gd
+++ core/vrm_instance.gd
@@ -5,98 +5,101 @@ extends Node
 const vrm_meta_class = preload("./vrm_meta.gd")
 
 # Reference to the secondary node if it exists.
-# Reference to the secondary node if it exists.
 var secondary_node: Node3D:
 	set(value):
 		secondary_node = value
 		if secondary_node:
-			if not _temp_spring_bones.is_empty():
-				secondary_node.set("spring_bones", _temp_spring_bones)
-				_temp_spring_bones.clear()
-			if not _temp_collider_groups.is_empty():
-				secondary_node.set("collider_groups", _temp_collider_groups)
-				_temp_collider_groups.clear()
-			if not _temp_collider_library.is_empty():
-				secondary_node.set("collider_library", _temp_collider_library)
-				_temp_collider_library.clear()
+			# Sync arrays to secondary_node
+			var parent_springs = spring_bones
+			var child_springs = secondary_node.get("spring_bones")
+			if child_springs is Array:
+				if parent_springs.is_empty() and not child_springs.is_empty():
+					spring_bones = child_springs
+				elif not parent_springs.is_empty() and child_springs.is_empty():
+					secondary_node.set("spring_bones", parent_springs)
+				elif parent_springs != child_springs:
+					secondary_node.set("spring_bones", parent_springs)
+
+			var parent_cgroups = collider_groups
+			var child_cgroups = secondary_node.get("collider_groups")
+			if child_cgroups is Array:
+				if parent_cgroups.is_empty() and not child_cgroups.is_empty():
+					collider_groups = child_cgroups
+				elif not parent_cgroups.is_empty() and child_cgroups.is_empty():
+					secondary_node.set("collider_groups", parent_cgroups)
+				elif parent_cgroups != child_cgroups:
+					secondary_node.set("collider_groups", parent_cgroups)
+
+			var parent_clib = collider_library
+			var child_clib = secondary_node.get("collider_library")
+			if child_clib is Array:
+				if parent_clib.is_empty() and not child_clib.is_empty():
+					collider_library = child_clib
+				elif not parent_clib.is_empty() and child_clib.is_empty():
+					secondary_node.set("collider_library", parent_clib)
+				elif parent_clib != child_clib:
+					secondary_node.set("collider_library", parent_clib)
 
 @export var vrm_meta: Resource
 
-# Temporary properties storage when secondary_node is not yet assigned
-var _temp_spring_bones: Array[VRMSpringBone] = []
-var _temp_collider_groups: Array[VRMColliderGroup] = []
-var _temp_collider_library: Array[VRMCollider] = []
-
-
-func _get_property_list() -> Array[Dictionary]:
-	var list: Array[Dictionary] = []
-	list.append({"name": "Spring bones", "type": TYPE_NIL, "usage": PROPERTY_USAGE_CATEGORY})
-	list.append(
-		{
-			"name": "spring_bones",
-			"type": TYPE_ARRAY,
-			"hint": PROPERTY_HINT_TYPE_STRING,
-			"hint_string": "%d/%d:VRMSpringBone" % [TYPE_OBJECT, TYPE_OBJECT],
-			"usage": PROPERTY_USAGE_DEFAULT
-		}
-	)
-	list.append(
-		{
-			"name": "collider_groups",
-			"type": TYPE_ARRAY,
-			"hint": PROPERTY_HINT_TYPE_STRING,
-			"hint_string": "%d/%d:VRMColliderGroup" % [TYPE_OBJECT, TYPE_OBJECT],
-			"usage": PROPERTY_USAGE_DEFAULT
-		}
-	)
-	list.append(
-		{
-			"name": "collider_library",
-			"type": TYPE_ARRAY,
-			"hint": PROPERTY_HINT_TYPE_STRING,
-			"hint_string": "%d/%d:VRMCollider" % [TYPE_OBJECT, TYPE_OBJECT],
-			"usage": PROPERTY_USAGE_DEFAULT
-		}
-	)
-	return list
-
-
-func _set(property: StringName, value: Variant) -> bool:
-	match property:
-		&"spring_bones":
-			if secondary_node:
+@export_category("Spring bones")
+@export var spring_bones: Array[VRMSpringBone] = []:
+	set(value):
+		spring_bones = value
+		if secondary_node:
+			var child_springs = secondary_node.get("spring_bones")
+			if child_springs is Array and not child_springs.is_empty() and value.is_empty():
+				spring_bones = child_springs
+			elif secondary_node.get("spring_bones") != value:
 				secondary_node.set("spring_bones", value)
-			else:
-				_temp_spring_bones = value
-			return true
-		&"collider_groups":
-			if secondary_node:
+
+@export var collider_groups: Array[VRMColliderGroup] = []:
+	set(value):
+		collider_groups = value
+		if secondary_node:
+			var child_cgroups = secondary_node.get("collider_groups")
+			if child_cgroups is Array and not child_cgroups.is_empty() and value.is_empty():
+				collider_groups = child_cgroups
+			elif secondary_node.get("collider_groups") != value:
 				secondary_node.set("collider_groups", value)
-			else:
-				_temp_collider_groups = value
-			return true
-		&"collider_library":
-			if secondary_node:
+
+@export var collider_library: Array[VRMCollider] = []:
+	set(value):
+		collider_library = value
+		if secondary_node:
+			var child_clib = secondary_node.get("collider_library")
+			if child_clib is Array and not child_clib.is_empty() and value.is_empty():
+				collider_library = child_clib
+			elif secondary_node.get("collider_library") != value:
 				secondary_node.set("collider_library", value)
-			else:
-				_temp_collider_library = value
-			return true
-	return false
+
+@export var recreate_spring_bone_simulator: bool = false:
+	set(value):
+		if value:
+			recreate_spring_bone_simulator = false
+			recreate_simulator()
 
 
-func _get(property: StringName) -> Variant:
-	match property:
-		&"spring_bones":
-			return secondary_node.get("spring_bones") if secondary_node else _temp_spring_bones
-		&"collider_groups":
-			return (
-				secondary_node.get("collider_groups") if secondary_node else _temp_collider_groups
-			)
-		&"collider_library":
-			return (
-				secondary_node.get("collider_library") if secondary_node else _temp_collider_library
-			)
-	return null
+func recreate_simulator() -> void:
+	if secondary_node:
+		# Sync parent arrays to child if child is empty
+		if secondary_node.get("spring_bones").is_empty() and not spring_bones.is_empty():
+			secondary_node.set("spring_bones", spring_bones)
+		if secondary_node.get("collider_groups").is_empty() and not collider_groups.is_empty():
+			secondary_node.set("collider_groups", collider_groups)
+		if secondary_node.get("collider_library").is_empty() and not collider_library.is_empty():
+			secondary_node.set("collider_library", collider_library)
+
+		# Sync child arrays to parent if parent is empty
+		if spring_bones.is_empty() and not secondary_node.get("spring_bones").is_empty():
+			spring_bones = secondary_node.get("spring_bones")
+		if collider_groups.is_empty() and not secondary_node.get("collider_groups").is_empty():
+			collider_groups = secondary_node.get("collider_groups")
+		if collider_library.is_empty() and not secondary_node.get("collider_library").is_empty():
+			collider_library = secondary_node.get("collider_library")
+
+		secondary_node.call("_ready")
+		notify_property_list_changed()
 
 
 @export_category("VRM Settings")
