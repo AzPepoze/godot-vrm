b90521dbf79b7397106f2d4ebbf9181bd374183d
AzPepoze <azpepoze@gmail.com>
2026-06-05 19:36:47 +0700

    feat: add import option to wrap imported VRM models in an armature

--- import_vrm.gd
+++ import_vrm.gd
@@ -21,9 +21,24 @@ func _import_scene(path: String, flags: int, options: Dictionary) -> Object:
     var vrm_extension: GLTFDocumentExtension = gltf_document_extension_class.new()
     gltf.register_gltf_document_extension(vrm_extension, true)
     var state: GLTFState = GLTFState.new()
+
+    var use_global: bool = options.get(&"vrm/use_global_defaults", true) as bool
+
+    var head_hiding: int = options.get(&"vrm/head_hiding_method", 0) as int
+    var bone_rename: int = options.get(&"vrm/bone_rename", 0) as int
+    var skeleton_name: String = options.get(&"vrm/skeleton_name", "GeneralSkeleton") as String
+    var wrap_armature: bool = options.get(&"vrm/wrap_in_armature", false) as bool
+    var remove_end: bool = options.get(&"vrm/remove_end_bones", true) as bool
+
+    if use_global:
+        head_hiding = ProjectSettings.get_setting("vrm/import/head_hiding_method", head_hiding)
+        bone_rename = ProjectSettings.get_setting("vrm/import/bone_rename", bone_rename)
+        skeleton_name = ProjectSettings.get_setting("vrm/import/skeleton_name", skeleton_name)
+        wrap_armature = ProjectSettings.get_setting("vrm/import/wrap_in_armature", wrap_armature)
+        remove_end = ProjectSettings.get_setting("vrm/import/remove_end_bones", remove_end)
+
     state.set_additional_data(
-        &"vrm/head_hiding_method",
-        options.get(&"vrm/head_hiding_method", 0) as vrm_constants.HeadHidingSetting
+        &"vrm/head_hiding_method", head_hiding as vrm_constants.HeadHidingSetting
     )
     state.set_additional_data(
         &"vrm/first_person_layers",
@@ -33,16 +48,10 @@ func _import_scene(path: String, flags: int, options: Dictionary) -> Object:
         &"vrm/third_person_layers",
         options.get(&"vrm/only_if_head_hiding_uses_layers/third_person_layers", 4) as int
     )
-    state.set_additional_data(
-        &"vrm/remove_end_bones", options.get(&"vrm/remove_end_bones", true) as bool
-    )
-    state.set_meta(&"vrm_bone_rename", options.get(&"vrm/bone_rename", 0) as int)
-    state.set_additional_data(
-        &"vrm/skeleton_name", options.get(&"vrm/skeleton_name", "GeneralSkeleton") as String
-    )
-    state.set_additional_data(
-        &"vrm/wrap_in_armature", options.get(&"vrm/wrap_in_armature", false) as bool
-    )
+    state.set_additional_data(&"vrm/remove_end_bones", remove_end)
+    state.set_meta(&"vrm_bone_rename", bone_rename)
+    state.set_additional_data(&"vrm/skeleton_name", skeleton_name)
+    state.set_additional_data(&"vrm/wrap_in_armature", wrap_armature)
     # HANDLE_BINARY_EMBED_AS_BASISU crashes on some files in 4.0 and 4.1
     state.handle_binary_image = GLTFState.HANDLE_BINARY_EMBED_AS_UNCOMPRESSED # GLTFState.HANDLE_BINARY_EXTRACT_TEXTURES
     VRMLogger.info("import_vrm.gd", "_import_scene: importing %s" % path)
